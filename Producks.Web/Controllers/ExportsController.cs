ae67f25f20c205975886bc5fa66a282b75b672f6
Stuart Gleeson <stuart.gleeson98@outlook.com>
2020-10-01 17:14:48 +0100

    Updated exports controller and added store controller.

--- Producks.Web/Controllers/ExportsController.cs
+++ Producks.Web/Controllers/ExportsController.cs
@@ -24,14 +24,51 @@ namespace Producks.Web.Controllers
         public async Task<IActionResult> GetBrands()
         {
             var brands = await _context.Brands
-                                       .Select(b => new BrandDto
-                                       {
-                                           Id = b.Id,
-                                           Name = b.Name,
-                                           Active = b.Active
-                                       })
-                                       .ToListAsync();
+                .Where(x => x.Active)
+                .Select(b => new BrandDto
+                {
+                    Id = b.Id,
+                    Name = b.Name
+                })
+                .ToListAsync();
             return Ok(brands);
+        }
+
+        // Get: api/Categories
+        [HttpGet("api/Categories")]
+        public async Task<IActionResult> GetCategories()
+        {
+            var categories = await _context.Categories
+                .Where(x => x.Active)
+                .Select(x => new CategoryDto
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Description = x.Description
+                }).ToListAsync();
+            return Ok(categories);
+        }
+
+        // Get: api/Product/{categoryId}/{brandId}
+        [HttpGet("api/Product/{categoryId}/{brandId}")]
+        public async Task<IActionResult> GetProduct(int categoryId, int brandId)
+        {
+            var products = await _context.Products
+                .Where(x =>
+                    x.BrandId == brandId &&
+                    x.CategoryId == categoryId &&
+                    x.Active)
+                .Select(x => new ProductDto
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Description = x.Description,
+                    Price = x.Price,
+                    StockLevel = x.StockLevel,
+                    BrandId = x.BrandId,
+                    CategoryId = x.CategoryId
+                }).ToListAsync();
+            return Ok(products);
         }
     }
 }
---
+++ Producks.Web/Controllers/StoreController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Producks.Data;
+using Producks.Web.Models;
+
+namespace Producks.Web.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly StoreDb _context;
+
+        public StoreController(StoreDb context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories
+                .Where(x => x.Active)
+                .ToListAsync());
+        }
+
+
+        //Id = Category Id
+        public async Task<IActionResult> ViewProducts(int? Id)
+        {
+            if (Id == null)
+                return BadRequest();
+
+            if (_context.Categories.FirstOrDefault(x => x.Id == Id) == null)
+                return NotFound();
+
+            var products = await _context.Products
+                .Where(x =>
+                    x.CategoryId == Id &&
+                    x.Active)
+                .Include(x => x.Category)
+                .Include(x => x.Brand)
+                .Select(x => new ProductDto
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Description = x.Description,
+                    Price = x.Price,
+                    StockLevel = x.StockLevel,
+                    BrandId = x.BrandId,
+                    BrandName = x.Brand.Name,
+                    CategoryId = x.CategoryId,
+                    CategoryName = x.Category.Name
+                })
+                .ToListAsync();
+
+            return View("Products", products);
+        }
+    }
+}
